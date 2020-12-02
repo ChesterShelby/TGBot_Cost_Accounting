b7e8b656341919113f1c89920403c6b254127398
Vadim Nazarov <NazarovvorazaN@mail.ru>
2020-12-02 17:55:15 +0300

    modified README.md and do echo bot in main fail

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-import aiogram
-import datetime
+import logging
+import telebot
+from aiogram import Bot, Dispatcher, executor, types
 from information import TOKEN_BOT
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=TOKEN_BOT)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я твой личный помошник по учету твоих финансов! \n"
+                        "Не бойся, я не расскажу твоей маме что ты покупаешь пиво))))")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
+
