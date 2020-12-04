20242a819f25867c3353e305e1ec2364884526ef
Vadim Nazarov <NazarovvorazaN@mail.ru>
2020-12-04 22:33:27 +0300

    Add proba file

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-import logging
 import telebot
-from aiogram import Bot, Dispatcher, executor, types
 from information import TOKEN_BOT
-logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=TOKEN_BOT)
-dp = Dispatcher(bot)
-
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    await message.reply("Привет! Я твой личный помошник по учету твоих финансов! \n"
-                        "Не бойся, я не расскажу твоей маме что ты покупаешь пиво))))")
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+bot = telebot.TeleBot(TOKEN_BOT)
+print(TOKEN_BOT)
 
 
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    print('Bot start')
+    bot.send_message(message.chat.id, 'Привет! Я твой личный помошник по учету твоих финансов! \n'
+                        'Не бойся, я не расскажу твоей маме что ты покупаешь пиво))))')
+
+
+bot.polling(none_stop=True, interval=0)
