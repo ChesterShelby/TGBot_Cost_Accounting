487eefa843984f7d994405de1293d1001d7e1b43
Vadim Nazarov <NazarovvorazaN@mail.ru>
2020-12-08 00:43:07 +0300

    I AM GENIUS!!! FIXED BUGS!!!!!!

--- registration_users.py
+++ registration_users.py
@@ -1,31 +1,19 @@
 import sqlite3
+import telebot
+from information import TOKEN_BOT
+bot = telebot.TeleBot(TOKEN_BOT)
 
 
 class RegistRation:
 
-    def __init__(self):
+    def adduser(self, user_id, message):
         self.connection = sqlite3.connect('db.db')
         self.cursor = self.connection.cursor()
-
-    def user_exists(self, user_id):
-        self.connection = sqlite3.connect('db.db')
-        self.cursor = self.connection.cursor()
-        with self.connection:
-            result = self.cursor.execute("SELECT * FROM 'users' WHERE 'user_id' = ?", (user_id,)).fetchall()
-            return bool(result)
-
-    def add_user(self, user_id):
-        self.connection = sqlite3.connect('db.db')
-        self.cursor = self.connection.cursor()
-        with self.connection:
-            return self.cursor.execute("INSERT INTO 'users' ('user_id') VALUES (?)", (user_id,))
-
-    def update_users(self, user_id):
-        self.connection = sqlite3.connect('db.db')
-        self.cursor = self.connection.cursor()
-        return self.cursor.execute("UPDATE 'users' SET 'status' = ? WHERE 'useer_id' = ?", (user_id,))
-
-    def close(self):
-        self.connection = sqlite3.connect('db.db')
-        self.cursor = self.connection.cursor()
-        self.connection.close()
+        self.cursor.execute("SELECT user_id FROM users")
+        if self.cursor.fetchone() is None:
+            self.cursor.execute("INSERT INTO 'users' ('user_id') VALUES (?)", (user_id,))
+            bot.send_message(message.chat.id, "Вы зарегистрированы")
+        else:
+            bot.send_message(message.chat.id, "Вы уже зарегистрированы")
+        self.connection.commit()
+        self.cursor.close()
